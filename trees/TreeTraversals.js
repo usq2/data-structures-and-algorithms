97f839fbbcb7c6abe9a5d9f3ead6085932b00f01
Utkarsh Sahay <utkarsh.sahay@tallysolutions.com>
2026-02-26 13:20:01 +0530

    Trees Basic Problems

--- trees/TreeTraversals.js
+++ trees/TreeTraversals.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var tree_1 = require("./tree");
 // DFS traversals
 //left->root->right ^
 var Inorder = function (pRoot) {
@@ -43,20 +42,45 @@ var LevelOrder = function (pRoot) {
             queue.unshift(curr.right);
     }
 };
-// 1
-// 2       3
-// 4       5       6
-var root = new tree_1.TreeNode(1, null, null);
-root.left = new tree_1.TreeNode(2, null, null);
-root.right = new tree_1.TreeNode(3, null, null);
-root.left.left = new tree_1.TreeNode(4, null, null);
-root.left.right = new tree_1.TreeNode(5, null, null);
-root.right.right = new tree_1.TreeNode(6, null, null);
-console.log("Inorder: \n");
-Inorder(root);
-console.log("\nPreorder: \n");
-Preorder(root);
-console.log("\nPostOrder: \n");
-Postorder(root);
-console.log("\nLevelOrder: \n");
-LevelOrder(root);
+var arr = [1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9];
+var PrintInorderFromArr = function (pArr) {
+    var stack = [0];
+    while (stack.length > 0) {
+        var curr = stack[stack.length - 1];
+        // if pArr[i].left is null, pop from stack and print
+        if (!pArr[(curr + 1) * 2 - 1]) {
+          var elem = stack.pop();
+          console.log(pArr[elem] + "   ", curr);
+          // if pArr[i].right then push that to stack
+          if (pArr[((elem + 1) * 2 + 1) -1]) {
+            stack.push(((elem + 1) * 2 + 1) -1);
+          } else {
+            stack.pop()
+          }
+        }
+        else {
+            stack.push(((curr + 1) * 2) - 1);
+            continue;
+        }
+       
+        stack.forEach(function (elem) { return console.log(elem); });
+    }
+};
+PrintInorderFromArr(arr);
+//         // 1
+//     // 2       3
+// // 4       5       6
+// const root = new TreeNode(1, null, null)
+// root.left = new TreeNode(2, null, null)
+// root.right = new TreeNode(3, null, null)
+// root.left.left = new TreeNode(4, null, null)
+// root.left.right = new TreeNode (5, null, null)
+// root.right.right = new TreeNode(6, null, null)
+// console.log ("Inorder: \n")
+// Inorder(root);
+// console.log ("\nPreorder: \n")
+// Preorder(root);
+// console.log ("\nPostOrder: \n")
+// Postorder(root);
+// console.log ("\nLevelOrder: \n")
+// LevelOrder(root)
--- trees/TreeTraversals.ts
+++ trees/TreeTraversals.ts
@@ -50,22 +50,47 @@ const LevelOrder = (pRoot:TreeNode|null) => {
 }
 
 
+const arr = [1,2,3,4,5,null,8,null,null,6,7,9]
 
-        // 1
-    // 2       3
-// 4       5       6
-const root = new TreeNode(1, null, null)
-root.left = new TreeNode(2, null, null)
-root.right = new TreeNode(3, null, null)
-root.left.left = new TreeNode(4, null, null)
-root.left.right = new TreeNode (5, null, null)
+const PrintInorderFromArr = (pArr: Array<number|null>) => {
 
-root.right.right = new TreeNode(6, null, null)
-console.log ("Inorder: \n")
-Inorder(root);
-console.log ("\nPreorder: \n")
-Preorder(root);
-console.log ("\nPostOrder: \n")
-Postorder(root);
-console.log ("\nLevelOrder: \n")
-LevelOrder(root)
+  let stack = [0]
+  while (stack.length > 0) {
+    
+    let curr = stack[stack.length-1];
+    // if pArr[i].left is null, pop from stack and print
+    if (!pArr[(curr+1)*2]) {
+      let elem = stack.pop ()!
+      console.log (pArr[elem] + "   ", curr)
+    } else {
+      console.log("pushing ", (curr+1)*2)
+      stack.push ((curr+1)*2)
+    }
+    // if pArr[i].right then push that to stack
+    if (pArr[(curr+1)*2 + 1]) {
+      stack.push ((curr+1)*2 + 1)
+    }
+    stack.forEach ((elem)=> console.log (elem))
+  }
+}
+
+PrintInorderFromArr(arr)
+
+//         // 1
+//     // 2       3
+// // 4       5       6
+// const root = new TreeNode(1, null, null)
+// root.left = new TreeNode(2, null, null)
+// root.right = new TreeNode(3, null, null)
+// root.left.left = new TreeNode(4, null, null)
+// root.left.right = new TreeNode (5, null, null)
+
+// root.right.right = new TreeNode(6, null, null)
+// console.log ("Inorder: \n")
+// Inorder(root);
+// console.log ("\nPreorder: \n")
+// Preorder(root);
+// console.log ("\nPostOrder: \n")
+// Postorder(root);
+// console.log ("\nLevelOrder: \n")
+// LevelOrder(root)
